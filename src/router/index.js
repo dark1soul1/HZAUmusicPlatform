bebaf6e1a83aef45860401ccf541db4d10f97657
dark1soul1 <627602537@qq.com>
2024-12-09 21:38:05 +0800

    perf:修改个人中心组件的路由，从views转至components

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,6 @@ const routes = [
   { path: '/home', name: 'Home', component:()=>import('../views/Home.vue'),},
   { path: '/song/:id', name: 'SongDetail', component:()=>import('../views/SongDetailPage.vue'),props:true, meta: { requiresAuth: true } },
   { path: '/mv/:id', name: 'MVDetail', component: ()=>import('../views/MVDetailPage.vue'),props:true, meta: { requiresAuth: true } },
-  { path: '/profile', name: 'Profile', component: ()=>import('../views/ProfilePage.vue'), meta: { requiresAuth: true } },
   { path: '/', redirect: '/home' }
 ];
 
